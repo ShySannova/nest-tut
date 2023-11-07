7afb1bd6d98bd4347b5ec38049fdfbb76597f1b6
ShySannova <sandeep09prasad@gmail.com>
2023-11-07 11:40:17 +0530

    lesson 02

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // handles /users route
+export class UsersController {
+  /*
+        GET /users
+        GET /users?role=value (query route)
+        GET /users/:id
+        POST /users
+        PATCH /users/:id
+        DELETE /users/:id
+    */
+
+  @Get() //GET /users or /users?role=value (query route)
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  //this route should be made before route with id so it does not conflict
+  @Get('interns') //GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() //POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
