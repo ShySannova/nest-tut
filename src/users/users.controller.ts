1bf4190df484968d08b4dcab22646bb5e3c2c738
ShySannova <sandeep09prasad@gmail.com>
2023-11-11 13:09:43 +0530

    lesson 03

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,8 +9,12 @@ import {
   Query,
 } from '@nestjs/common';
 
+import { UsersService } from './users.service';
+
 @Controller('users') // handles /users route
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   /*
         GET /users
         GET /users?role=value (query route)
@@ -22,32 +26,47 @@ export class UsersController {
 
   @Get() //GET /users or /users?role=value (query route)
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   //this route should be made before route with id so it does not conflict
   @Get('interns') //GET /users/interns
   findAllInterns() {
-    return [];
+    return this.usersService.findAllInterns();
   }
 
   @Get(':id') //GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() //POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') //PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id') //DELETE /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,101 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'San',
+      email: 'sandeep09prasad@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      name: 'Brown',
+      email: 'brown@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 3,
+      name: 'Joe',
+      email: 'joe@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 4,
+      name: 'Dave',
+      email: 'dave@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Kevin',
+      email: 'kevin@gmail.com',
+      role: 'INTERN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => {
+        return user.role === role;
+      });
+    }
+    return this.users;
+  }
+
+  findAllInterns() {
+    return this.users.filter((user) => {
+      return user.role === 'INTERN';
+    });
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => {
+      if (id === user.id) {
+        return user;
+      }
+    });
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const generateId = this.users[this.users.length - 1].id + 1;
+    const newUser = {
+      id: generateId,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...userUpdate };
+      }
+      return user;
+    });
+
+    return { id, ...userUpdate };
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => {
+      return user.id !== id;
+    });
+    return removedUser;
+  }
+}
